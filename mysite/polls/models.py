080c06e704a08aa614dcfe02a9ff1f9c9c989c88
John W. Clark <john.wilbur.clark@gmail.com>
2020-09-24 15:15:22 -0500

    changes to support the answers

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -8,7 +10,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now 
+    was_published_recently.admin_order_field = 'pub_date' 
+    was_published_recently.boolean = True 
+    was_published_recently.short_description = 'Published recently?'
 
 
 
@@ -18,3 +24,8 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+def was_published_recently(self):
+    now = timezone.now()
+    now_minus_one = now - datetime.timedelta(days=1)
+    return now_minus_one <= self.pub_date <= now
