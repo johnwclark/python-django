5a3876619c1e586fa16650663547fd8529ccb304
John W. Clark <john.wilbur.clark@gmail.com>
2020-09-28 11:55:43 -0500

    fixed the allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j@dre50m$t(b_@5sk-feu99b2gxcv@=2=@gt+r14uqa3dr(xg1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
